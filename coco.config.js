c1f09b66f230e05c84cc3107fbecbcf6ce24cb9f
1391020381 <1391020381@qq.com>
2022-06-06 16:04:28 +0800

    修改配置

--- coco.config.js
+++ coco.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  webDomian: 'https://justdoit.github.io/coco-global-banner',
+  webDomian: 'https://justdoit-h5.github.io/coco-global-banner',
   gitUrl: 'git@github.com:justdoit-h5/coco-global-banner.git',
   name: 'coco-global-banner',
   description: "远程banner组件"
